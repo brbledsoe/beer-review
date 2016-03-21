e8f41f2dc1fcf08520f60a3988abc78456b21bf9
Brandon Bledsoe <brbledsoe.m@gmail.com>
2016-03-21 19:56:43 +0200

    sucessfully implemented beer review up to excercise 4, now will try final excercise

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+var beers = [];
+
 $('.post-beer').on('click', function (e) {
   e.preventDefault();
+
+  var name = $('#beer-name').val();
+  var category = $('#beer-category').val();
+  var rating = $('#beer-rating').val();
+
+  addBeer(name, category, rating);
+  updateBeers();
+
 });
+
+var addBeer = function (name, category, rating){  
+  var br = {
+    name: name, 
+    category: category,
+    rating: rating,
+
+  };
+
+  beers.push(br);
+// {name: name, category: category});
+  }
+
+var updateBeers = function () {
+  $('.beers').find('p').remove();
+  for( var i = 0; i < beers.length; i++ ) {
+    $('.beers').append('<p>' + beers[i].name + ': ' + beers[i].category + ': ' + beers[i].rating + '</p>');
+    
+  }
+}
