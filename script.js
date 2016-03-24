0a5875d7ba3351cdce5e8f6a1c1fadd710ad9000
Brandon Bledsoe <brbledsoe.m@gmail.com>
2016-03-24 13:40:36 +0200

    created a closure function.  Made things more modular

--- script.js
+++ script.js
@@ -1,4 +1,45 @@
-var beers = [];
+var BeerReviewApp = function () {
+
+  var beers = [];
+
+  var sortBeer = function () {
+    beers.sort(function(a, b){
+      return a.rating - b.rating;
+      console.log(beers);
+      // var a1= a.name, b1= b.name;
+      // if(a1== b1) return 0;
+      // return a1> b1? 1: -1;
+    });
+  }
+
+  var addBeer = function (name, category, rating){  
+    var br = {
+      name: name, 
+      category: category,
+      rating: rating,
+
+    };
+
+    beers.push(br);
+  // {name: name, category: category});
+    }
+
+  var updateBeers = function () {
+    $('.beers').find('p').remove();
+    for( var i = 0; i < beers.length; i++ ) {
+      $('.beers').append('<p>' + beers[i].name + ': ' + beers[i].category + ': ' + beers[i].rating + '</p>');
+        
+      }
+    }
+
+  return {
+    addBeer: addBeer,
+    updateBeers: updateBeers
+  }
+
+} 
+
+var beer = BeerReviewApp();
 
 // CLICK FUNCTION FOR POSTING
 $('.post-beer').on('click', function (e) {
@@ -8,51 +49,20 @@ $('.post-beer').on('click', function (e) {
   var category = $('#beer-category').val();
   var rating = $('#beer-rating').val();
 
-  addBeer(name, category, rating);
-  updateBeers();
+  beer.addBeer(name, category, rating);
+  beer.updateBeers();
 
-});
+  });
 
 // CLICK FUNCTION FOR SORTING
 $('.sort-beer').on('click', function (e) {
   e.preventDefault();
 
-  updateBeers();
-  sortBeer();
-});
-
-var sortBeer = function () {
-  beers.sort(function(a, b){
-    return a.rating - b.rating;
-    console.log(beers);
-    // var a1= a.name, b1= b.name;
-    // if(a1== b1) return 0;
-    // return a1> b1? 1: -1;
-});
-}
+  beer.updateBeers();
+  beer.sortBeer();
+  });
 
 
 
-
-
-var addBeer = function (name, category, rating){  
-  var br = {
-    name: name, 
-    category: category,
-    rating: rating,
-
-  };
-
-  beers.push(br);
-// {name: name, category: category});
-  }
-
-var updateBeers = function () {
-  $('.beers').find('p').remove();
-  for( var i = 0; i < beers.length; i++ ) {
-    $('.beers').append('<p>' + beers[i].name + ': ' + beers[i].category + ': ' + beers[i].rating + '</p>');
-    
-  }
-}
 
 
